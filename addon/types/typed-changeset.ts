cf8e8a8f50284abed4f83914aff9f633dc2842a1
AmauryD <deflorenne.amaury@gmail.com>
2021-07-27 11:12:53 +0200

    more types to changeset

--- addon/types/typed-changeset.ts
+++ addon/types/typed-changeset.ts
@@ -2,6 +2,8 @@ import { BufferedChangeset } from 'ember-changeset/types';
 
 export interface TypedBufferedChangeset<T extends Record<string, any>>
   extends BufferedChangeset {
+  pendingData: Partial<T>;
+  data: T;
   // eslint-disable-next-line no-unused-vars
   get<K extends keyof T>(key: K): T[K];
   // eslint-disable-next-line no-unused-vars
