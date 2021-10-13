b7feed0f6287b651ad634a4a361bdc1a45626969
AmauryD <deflorenne.amaury@gmail.com>
2021-10-13 14:03:40 +0200

    test generation

---
+++ tests/integration/components/comments-test.ts
@@ -0,0 +1,78 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+import { Changeset } from 'ember-changeset';
+import lookupValidator from 'ember-changeset-validations';
+import Validation from 'dummy/validator/forms/comments';
+import { TypedBufferedChangeset } from 'ember-form-changeset-validations';
+import { CommentsDTO } from 'dummy/pods/components/forms/articles/component';
+import click from '@ember/test-helpers/dom/click';
+import fillIn from '@ember/test-helpers/dom/fill-in';
+
+module('Integration | Component | Comment', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('Create (empty changeset)', async function (assert) {
+    assert.expect(3);
+
+    this.set(
+      'changeset',
+      Changeset(
+        {} as Record<keyof typeof Validation, any>,
+        lookupValidator(Validation),
+        Validation
+      )
+    );
+
+    this.set(
+      'saveFunction',
+      (changeset: TypedBufferedChangeset<CommentsDTO>) => {
+        assert.strictEqual(changeset.get('content'), 'text');
+      }
+    );
+
+    await render(
+      hbs`<Forms::Comments @changeset={{this.changeset}} @saveFunction={{this.saveFunction}}/>`
+    );
+
+    await click("button[type='submit']");
+    assert.dom("button[type='submit']").isDisabled();
+
+    assert.dom('#aaaa').hasValue('');
+
+    await fillIn('#aaaa', 'text');
+
+    await click("button[type='submit']");
+  });
+
+  test('Edit (populated changeset)', async function (assert) {
+    assert.expect(2);
+
+    this.set(
+      'changeset',
+      Changeset(
+        {
+          content: 'text',
+        } as Record<keyof typeof Validation, any>,
+        lookupValidator(Validation),
+        Validation
+      )
+    );
+
+    this.set(
+      'saveFunction',
+      (changeset: TypedBufferedChangeset<CommentsDTO>) => {
+        assert.strictEqual(changeset.get('content'), 'text');
+      }
+    );
+
+    await render(
+      hbs`<Forms::Comments @saveFunction={{this.saveFunction}} @changeset={{this.changeset}}/>`
+    );
+
+    assert.dom('#aaaa').hasValue('text');
+
+    await click("button[type='submit']");
+  });
+});
