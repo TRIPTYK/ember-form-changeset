d570aac6071478c7843b8f8aa26e8f149092fc40
AmauryD <deflorenne.amaury@gmail.com>
2021-10-18 09:54:37 +0200

    version

--- tests/test-helper.ts
+++ tests/test-helper.ts
@@ -1,7 +1,9 @@
+// @ts-ignore
 import Application from 'dummy/app';
 import config from 'dummy/config/environment';
 import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
+import 'qunit-dom';
 import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
 
