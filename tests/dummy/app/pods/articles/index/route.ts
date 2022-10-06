3dac73f737861e60c1189813e7dd80d95a079782
AmauryD <deflorenne.amaury@gmail.com>
2022-10-06 22:59:46 +0200

    add big changeset manipulation methods

--- tests/dummy/app/pods/articles/index/route.ts
+++ tests/dummy/app/pods/articles/index/route.ts
@@ -1,11 +1,11 @@
-import Route from '@ember/routing/route';
+import Route, { RouteModel } from '@ember/routing/route';
 import type Store from '@ember-data/store';
 import { inject } from '@ember/service';
 
 export default class ArticlesIndex extends Route {
   @inject declare store: Store;
 
-  model() {
+  model(): RouteModel {
     return this.store.findAll('article', {
       include: 'comments,author,image',
     });
