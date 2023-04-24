1297523144db6c888892ebfc1d4d3e1875c00599
AmauryD <deflorenne.amaury@gmail.com>
2023-04-24 09:09:18 +0200

    remove .d.ts

--- changeset/create-changeset.d.ts
+++
@@ -1,9 +0,0 @@
-import { Changeset, ProxyWrappedChangeset } from 'ember-form-changeset-validations/types/typed-changeset';
-import { ConstructorParams } from 'expect-type';
-import { Class } from 'type-fest';
-/**
- * Creates a changeset and wrap it into a proxy
- * If a proxy is not required, just use new changeset(...)
- */
-export declare function createChangeset<C extends Changeset>(changesetClass: Class<C>, ...args: ConstructorParams<Class<C>>): ProxyWrappedChangeset<C>;
-//# sourceMappingURL=create-changeset.d.ts.map
